a4232db75139baef17fcce91cb9f5de167a4dad0
shahbaz <shahbaz.uollhr@gmail.com>
2017-12-26 17:06:47 +0500

    updated the imported excel file dir

--- app/uploaders/excel_import_uploader.rb
+++ app/uploaders/excel_import_uploader.rb
@@ -11,7 +11,7 @@ class ExcelImportUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/"
+    "#{Rails.root}/transactions_imported"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
