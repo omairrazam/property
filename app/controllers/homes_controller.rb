4e646261a08fbf4d2d94979b84f269250b22470d
yaseen iqbal <p136011@nu.edu.pk>
2017-12-26 20:43:48 +0500

    Setup dashboard buying & selling data table child rows.

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,5 +1,12 @@
 class HomesController < ApplicationController
   def index
-  	Transaction.modes.keys.each { |mode| instance_variable_set("@#{mode}", ::DailyTransactionProcessor.new(mode)) }
+  	nature = params[:nature] || Transaction.natures.keys.first 
+  	mode = params[:mode] || Transaction.modes.keys.second  
+  	#Transaction.modes.keys.each { |mode| instance_variable_set("@#{mode}", ::DailyTransactionProcessor.new(mode)) }
+    @data = Transaction.public_send('current_'+ mode, nature).only_parents.includes(:children)
+    respond_to do |format|
+        format.html 
+        format.json { render json: {data:@data.as_json(include: [:children,:category]) }}
+    end
   end
 end
